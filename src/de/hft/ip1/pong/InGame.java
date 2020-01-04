2d896d6a5431df2f253105bc1fef20602560acc4
StuttgarterBenny <56401307+StuttgarterBenny@users.noreply.github.com>
2020-01-04 19:39:31 +0100

    Add files via upload

--- src/de/hft/ip1/pong/InGame.java
+++ src/de/hft/ip1/pong/InGame.java
@@ -1,10 +1,25 @@
 package de.hft.ip1.pong;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.JFrame;
+
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+
 import static de.hft.ip1.pong.Application.ROOTFRAME;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 import static de.hft.ip1.pong.Application.PONG;
 
 public class InGame {
+
 	public InGame() {
 		
 		ROOTFRAME.setSize(800, 600);
@@ -20,6 +35,8 @@ public class InGame {
 		lbldraw.setBounds(0,0, ROOTFRAME.getWidth(), ROOTFRAME.getWidth());
 		lbldraw.setVisible(true);
 		ROOTFRAME.add(lbldraw);
+		
 	}
+	
 }
 
