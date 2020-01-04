b0664a69abe4815b0a625c315c83267249f6e86a
StuttgarterBenny <56401307+StuttgarterBenny@users.noreply.github.com>
2020-01-04 19:38:35 +0100

    Add files via upload

---
+++ src/de/hft/ip1/pong/Musik.java
@@ -0,0 +1,38 @@
+package de.hft.ip1.pong;
+
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class Musik {
+	
+public static synchronized void music(String track) {
+	
+	final String trackname = track;
+	
+	new Thread (new Runnable() {
+		
+		@Override
+		public void run() {
+			while (true) {
+				
+				try {
+					
+					Clip clip = AudioSystem.getClip();
+					AudioInputStream inputstream = AudioSystem.getAudioInputStream(new File (trackname));
+					clip.open(inputstream);
+					clip.loop(clip.LOOP_CONTINUOUSLY);
+					
+					Thread.sleep(clip.getMicrosecondLength()/1000);
+	
+				}catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			
+		}
+	}).start();
+}
+}
