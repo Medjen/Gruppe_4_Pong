c0365074f5cde5f673c2adf168a16e70e67aed88
henry-hft <333333henry@gmail.com>
2020-01-05 11:35:33 +0100

    change the FontSize of the socre

--- src/de/hft/ip1/pong/Application.java
+++ src/de/hft/ip1/pong/Application.java
@@ -3,9 +3,26 @@ package de.hft.ip1.pong;
 import javax.swing.JFrame;
 
 public class Application {
+	/** Attributes */
+	/**
+	 * name of programs configuration files base folder<br />
+	 * So if you want to write some files onto the disk, please do it all in a
+	 * central place like this.
+	 */
+	protected static final String BASEFOLDER = System.getProperty("user.home") + "/.ip1ws1920/group_4";
+
+	/**
+	 * Root JFrame into which we put our JComponents we want to show.
+	 */
 	protected static final JFrame ROOTFRAME = new JFrame("Pong");
+
+	/**
+	 * Operation main
+	 *
+	 * @param args - CLI arguments
+	 */
 	public static void main(String[] args) {
-		
+
 		ROOTFRAME.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		ROOTFRAME.setLocation(600, 300);
 		ROOTFRAME.setResizable(false);
--- src/de/hft/ip1/pong/PlayFieldPanel.java
+++ src/de/hft/ip1/pong/PlayFieldPanel.java
@@ -5,37 +5,40 @@ import java.awt.*;
 import javax.swing.JLabel;
 import static de.hft.ip1.pong.Application.ROOTFRAME;
 import static de.hft.ip1.pong.InGame.PONG;
-public class PlayFieldPanel extends JLabel{
 
-		// TODO Auto-generated constructor stub
-	
+public class PlayFieldPanel extends JLabel {
 
-	protected void paintComponent(Graphics g){
-		
+	// TODO Auto-generated constructor stub
+
+	protected void paintComponent(Graphics g) {
+
 		super.paintComponent(g);
-		
+
 		Graphics2D g2d = (Graphics2D) g;
-		
+
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		
+
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, ROOTFRAME.getWidth(), ROOTFRAME.getHeight());
-		
+
 		g.setColor(Color.GREEN);
-		
-		for(int i = 0; i<= ROOTFRAME.getHeight()/20; i++){
-			g.fillRect(ROOTFRAME.getWidth()/2 -5, i * 20, 10, 10);
+
+		for (int i = 0; i <= ROOTFRAME.getHeight() / 20; i++) {
+			g.fillRect(ROOTFRAME.getWidth() / 2 - 5, i * 20, 10, 10);
 		}
-		
+
 		g.fillRect(PONG.getPlayerX(), PONG.getPlayerY(), 25, 150);
 		g.fillRect(PONG.getOpponentX(), PONG.getOpponentY(), 25, 150);
-		
-		g.drawString("" + PONG.getPlayerPoints(), ROOTFRAME.getWidth()/2 - 95, 75);
-		g.drawString("" + PONG.getOpponentPoints(), ROOTFRAME.getWidth()/2 + 50, 75);
-		
+
+		Font currentFont = g.getFont();
+		Font newFont = currentFont.deriveFont(currentFont.getSize() * 2.4F);
+		g.setFont(newFont);
+		g.drawString("" + PONG.getPlayerPoints(), ROOTFRAME.getWidth() / 2 - 75, 75);
+		g.drawString("" + PONG.getOpponentPoints(), ROOTFRAME.getWidth() / 2 + 50, 75);
+
 		g.fillRect(PONG.getPuckX(), PONG.getPuckY(), 20, 20);
-		
+
 		repaint();
-		
+
 	}
 }
